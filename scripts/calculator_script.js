365b2fa63c133aa5ae82fd94c2413e9bf601eb34
jpicks <pickering.jamesd@gmail.com>
2026-03-16 21:25:48 +0800

    Update documentation

--- scripts/calculator_script.js
+++ scripts/calculator_script.js
@@ -1,22 +1,22 @@
-var ndp = 3;
-var planck = 6.62607E-34;
-var c = 2.99792E8;
-var electronvolt = 1.60218E-19;
-var avogadro = 6.022E23;
-var hartree = 4.35974E-18;
-var kwh = 3.6E6;
-var kcal = 4.184;
-var boltz = 1.3806E-23;
-var faraday = 96485.3415;
-var pi = 3.14159;
-var btu = 1055;
-var tnt = 4184;
-var rydberg = 13.6056 * electronvolt;
-var erg = 1.0E-7;
-var booe = 6.0E9;
+"use strict";
+const ndp = 3;
+const planck = 6.62607E-34;
+const c = 2.99792E8;
+const electronvolt = 1.60218E-19;
+const avogadro = 6.022E23;
+const hartree = 4.35974E-18;
+const kwh = 3.6E6;
+const kcal = 4.184;
+const boltz = 1.3806E-23;
+const faraday = 96485.3415;
+const pi = 3.14159;
+const btu = 1055;
+const tnt = 4184;
+const rydberg = 13.6056 * electronvolt;
+const erg = 1.0E-7;
+const booe = 6.0E9;
 // helper function to format numbers so they're more readable
-function formatNumber(num, decimals) {
-    if (decimals === void 0) { decimals = ndp; }
+function formatNumber(num, decimals = ndp) {
     if (Math.abs(num) > 10000 || Math.abs(num) < 0.001) {
         return num.toExponential(decimals);
     }
@@ -29,69 +29,65 @@ function getBox(id) {
     return document.getElementById(id);
 }
 // frequency. E = hv
-var freqbox = getBox("freq");
-freqbox.toJoule = function () { return Number(freqbox.value) * planck; };
-freqbox.fromJoule = function (energyJ) { return energyJ / planck; };
+const freqbox = getBox("freq");
+freqbox.toJoule = () => Number(freqbox.value) * planck;
+freqbox.fromJoule = (energyJ) => energyJ / planck;
 // ang freq. E = h v / 2pi (omega = 2pi nu)
-var angfreqbox = getBox("angfreq");
-angfreqbox.toJoule = function () { return Number(freqbox.value) * planck / (2 * pi); };
-angfreqbox.fromJoule = function (energyJ) { return (2 * pi * energyJ) / planck; };
+const angfreqbox = getBox("angfreq");
+angfreqbox.toJoule = () => Number(freqbox.value) * planck / (2 * pi);
+angfreqbox.fromJoule = (energyJ) => (2 * pi * energyJ) / planck;
 // hartree. 1 hartree = 4.359E-18 J
-var hartreebox = getBox("energyH");
-hartreebox.toJoule = function () { return Number(hartreebox.value) * hartree; };
-hartreebox.fromJoule = function (energyJ) { return energyJ / hartree; };
+const hartreebox = getBox("energyH");
+hartreebox.toJoule = () => Number(hartreebox.value) * hartree;
+hartreebox.fromJoule = (energyJ) => energyJ / hartree;
 // kWh. 1 kWh = 1000 * 3600 J
-var kWhbox = getBox("energykWh");
-kWhbox.toJoule = function () { return Number(kWhbox.value) * kwh; };
-kWhbox.fromJoule = function (energyJ) { return energyJ / kwh; };
+const kWhbox = getBox("energykWh");
+kWhbox.toJoule = () => Number(kWhbox.value) * kwh;
+kWhbox.fromJoule = (energyJ) => energyJ / kwh;
 // Joules. base unit for conversion
-var energyJbox = getBox("energyJ");
-energyJbox.toJoule = function () { return Number(energyJbox.value); };
-energyJbox.fromJoule = function (energyJ) { return energyJ; };
+const energyJbox = getBox("energyJ");
+energyJbox.toJoule = () => Number(energyJbox.value);
+energyJbox.fromJoule = (energyJ) => energyJ;
 // eV. 1 eV = 1.6E-19 Joules
-var energyeVbox = getBox("energyeV");
-energyeVbox.toJoule = function () { return Number(energyeVbox.value) * electronvolt; };
-energyeVbox.fromJoule = function (energyJ) { return energyJ / electronvolt; };
+const energyeVbox = getBox("energyeV");
+energyeVbox.toJoule = () => Number(energyeVbox.value) * electronvolt;
+energyeVbox.fromJoule = (energyJ) => energyJ / electronvolt;
 // kJ/mol. 1 kJ/mol = (Joule / 1000) * avogadro
-var energykJmolbox = getBox("energykJmol");
-energykJmolbox.toJoule = function () { return (1000 * Number(energykJmolbox.value)) / avogadro; };
-energykJmolbox.fromJoule = function (energyJ) { return (energyJ * avogadro) / 1000; };
+const energykJmolbox = getBox("energykJmol");
+energykJmolbox.toJoule = () => (1000 * Number(energykJmolbox.value)) / avogadro;
+energykJmolbox.fromJoule = (energyJ) => (energyJ * avogadro) / 1000;
 // kcal/mol. 1 kcal/mol = (Joule / 1000) * avogadro / 4.184
-var energykcalmolbox = getBox("energykcalmol");
-energykcalmolbox.toJoule = function () {
-    return (1000 * kcal * Number(energykcalmolbox.value)) / avogadro;
-};
-energykcalmolbox.fromJoule = function (energyJ) { return (energyJ * avogadro) / (1000 * kcal); };
+const energykcalmolbox = getBox("energykcalmol");
+energykcalmolbox.toJoule = () => (1000 * kcal * Number(energykcalmolbox.value)) / avogadro;
+energykcalmolbox.fromJoule = (energyJ) => (energyJ * avogadro) / (1000 * kcal);
 // optical period. E = h/T
-var timebox = getBox("time");
-timebox.toJoule = function () { return planck / Number(timebox.value); };
-timebox.fromJoule = function (energyJ) { return planck / energyJ; };
+const timebox = getBox("time");
+timebox.toJoule = () => planck / Number(timebox.value);
+timebox.fromJoule = (energyJ) => planck / energyJ;
 // wavelength. E = hc/lambda
-var wavelengthbox = getBox("wavelength");
-wavelengthbox.toJoule = function () { return (planck * c) / Number(wavelengthbox.value); };
-wavelengthbox.fromJoule = function (energyJ) { return (planck * c) / energyJ; };
+const wavelengthbox = getBox("wavelength");
+wavelengthbox.toJoule = () => (planck * c) / Number(wavelengthbox.value);
+wavelengthbox.fromJoule = (energyJ) => (planck * c) / energyJ;
 // wavenumber. E = hc nubar
-var wavenumberbox = getBox("wavenumber");
-wavenumberbox.toJoule = function () { return planck * c * Number(wavenumberbox.value); };
-wavenumberbox.fromJoule = function (energyJ) { return energyJ / (planck * c); };
+const wavenumberbox = getBox("wavenumber");
+wavenumberbox.toJoule = () => planck * c * Number(wavenumberbox.value);
+wavenumberbox.fromJoule = (energyJ) => energyJ / (planck * c);
 // wavenumber in cm. E = 100 hc nubar
-var wavenumberboxcm = getBox("wavenumbercm");
-wavenumberboxcm.toJoule = function () { return planck * c * Number(wavenumberboxcm.value) * 100; };
-wavenumberboxcm.fromJoule = function (energyJ) { return energyJ / (planck * c * 100); };
+const wavenumberboxcm = getBox("wavenumbercm");
+wavenumberboxcm.toJoule = () => planck * c * Number(wavenumberboxcm.value) * 100;
+wavenumberboxcm.fromJoule = (energyJ) => energyJ / (planck * c * 100);
 // angular wavenumber. angnubar = 2 pi * nubar
-var angwavenumberbox = getBox("angwavenumber");
-angwavenumberbox.toJoule = function () {
-    return planck * c * (Number(angwavenumberbox.value) / (2 * pi));
-};
-angwavenumberbox.fromJoule = function (energyJ) { return (2 * pi * energyJ) / (planck * c); };
+const angwavenumberbox = getBox("angwavenumber");
+angwavenumberbox.toJoule = () => planck * c * (Number(angwavenumberbox.value) / (2 * pi));
+angwavenumberbox.fromJoule = (energyJ) => (2 * pi * energyJ) / (planck * c);
 // characteristic temperature. E = kT 
-var temperaturebox = getBox("temperature");
-temperaturebox.toJoule = function () { return Number(temperaturebox.value) * boltz; };
-temperaturebox.fromJoule = function (energyJ) { return energyJ / boltz; };
+const temperaturebox = getBox("temperature");
+temperaturebox.toJoule = () => Number(temperaturebox.value) * boltz;
+temperaturebox.fromJoule = (energyJ) => energyJ / boltz;
 // temperature for boltzmann, rate, equilibria calculations (i.e. ensemble temperature)
 // recalculate various parameters when this is changed
-var boltzmannTbox = getBox("boltzmannT");
-boltzmannTbox.changed = function (energyJ) {
+const boltzmannTbox = getBox("boltzmannT");
+boltzmannTbox.changed = (energyJ) => {
     boltzmannbox.value = formatNumber(Number(boltzmanngbox.value) *
         Math.exp(-energyJ / (boltz * Number(boltzmannTbox.value))));
     equilibriumconstantbox.value = formatNumber(Math.exp(-energyJ / (boltz * Number(boltzmannTbox.value))));
@@ -100,109 +96,85 @@ boltzmannTbox.changed = function (energyJ) {
         Math.exp(-energyJ / (boltz * Number(boltzmannTbox.value))));
 };
 // boltzmann degeneracy ratio (upper/lower)
-var boltzmanngbox = getBox("boltzmanng");
-boltzmanngbox.changed = function (energyJ) {
+const boltzmanngbox = getBox("boltzmanng");
+boltzmanngbox.changed = (energyJ) => {
     boltzmannbox.value = formatNumber(Number(boltzmanngbox.value) *
         Math.exp(-energyJ / (boltz * Number(boltzmannTbox.value))));
 };
 // gives boltzmann populations (upper/lower)
-var boltzmannbox = getBox("boltzmann");
-boltzmannbox.toJoule = function () {
-    return Math.log(Number(boltzmannbox.value) / Number(boltzmanngbox.value)) * boltz * Number(boltzmannTbox.value);
-};
-boltzmannbox.fromJoule = function (energyJ) {
-    return Number(boltzmanngbox.value) *
-        Math.exp(-energyJ / (boltz * Number(boltzmannTbox.value)));
-};
+const boltzmannbox = getBox("boltzmann");
+boltzmannbox.toJoule = () => Math.log(Number(boltzmannbox.value) / Number(boltzmanngbox.value)) * boltz * Number(boltzmannTbox.value);
+boltzmannbox.fromJoule = (energyJ) => Number(boltzmanngbox.value) *
+    Math.exp(-energyJ / (boltz * Number(boltzmannTbox.value)));
 // gives K = exp(-dG/kT)
-var equilibriumconstantbox = getBox("equilibrium");
-equilibriumconstantbox.toJoule = function () {
-    return -boltz *
-        Number(boltzmannTbox.value) *
-        Math.log(Number(equilibriumconstantbox.value));
-};
-equilibriumconstantbox.fromJoule = function (energyJ) {
-    return Math.exp(-energyJ / (boltz * Number(boltzmannTbox.value)));
-};
+const equilibriumconstantbox = getBox("equilibrium");
+equilibriumconstantbox.toJoule = () => -boltz *
+    Number(boltzmannTbox.value) *
+    Math.log(Number(equilibriumconstantbox.value));
+equilibriumconstantbox.fromJoule = (energyJ) => Math.exp(-energyJ / (boltz * Number(boltzmannTbox.value)));
 // gives lnK = -dG/kT
-var lnequilibriumconstantbox = getBox("lnequilibrium");
-lnequilibriumconstantbox.toJoule = function () {
-    return -boltz *
-        Number(boltzmannTbox.value) *
-        Number(lnequilibriumconstantbox.value);
-};
-lnequilibriumconstantbox.fromJoule = function (energyJ) {
-    return -energyJ / (boltz * Number(boltzmannTbox.value));
-};
+const lnequilibriumconstantbox = getBox("lnequilibrium");
+lnequilibriumconstantbox.toJoule = () => -boltz *
+    Number(boltzmannTbox.value) *
+    Number(lnequilibriumconstantbox.value);
+lnequilibriumconstantbox.fromJoule = (energyJ) => -energyJ / (boltz * Number(boltzmannTbox.value));
 // Arrhenius A factor
-var Aconstantbox = getBox("Aconstant");
-Aconstantbox.changed = function (energyJ) {
+const Aconstantbox = getBox("Aconstant");
+Aconstantbox.changed = (energyJ) => {
     arrheniusbox.value = formatNumber(Number(Aconstantbox.value) *
         Math.exp(-energyJ / (boltz * Number(boltzmannTbox.value))));
 };
 // Arrhenius rate constant, k = A exp(-Ea/RT)
-var arrheniusbox = getBox("arrhenius");
-arrheniusbox.toJoule = function () {
-    return -Math.log(Number(arrheniusbox.value) / Number(Aconstantbox.value)) *
-        boltz *
-        Number(boltzmannTbox.value);
-};
-arrheniusbox.fromJoule = function (energyJ) {
-    return Number(Aconstantbox.value) *
-        Math.exp(-energyJ / (boltz * Number(boltzmannTbox.value)));
-};
+const arrheniusbox = getBox("arrhenius");
+arrheniusbox.toJoule = () => -Math.log(Number(arrheniusbox.value) / Number(Aconstantbox.value)) *
+    boltz *
+    Number(boltzmannTbox.value);
+arrheniusbox.fromJoule = (energyJ) => Number(Aconstantbox.value) *
+    Math.exp(-energyJ / (boltz * Number(boltzmannTbox.value)));
 // half life from Arrhenius assuming 1st order kinetics t = ln2 / k 
-var halflifebox = getBox("halflife");
-halflifebox.toJoule = function () {
-    return -Math.log((Math.log(2) / Number(halflifebox.value)) / Number(Aconstantbox.value)) *
-        boltz *
-        Number(boltzmannTbox.value);
-};
-halflifebox.fromJoule = function (energyJ) {
-    return Math.log(2) /
-        (Number(Aconstantbox.value) *
-            Math.exp(-energyJ / (boltz * Number(boltzmannTbox.value))));
-};
+const halflifebox = getBox("halflife");
+halflifebox.toJoule = () => -Math.log((Math.log(2) / Number(halflifebox.value)) / Number(Aconstantbox.value)) *
+    boltz *
+    Number(boltzmannTbox.value);
+halflifebox.fromJoule = (energyJ) => Math.log(2) /
+    (Number(Aconstantbox.value) *
+        Math.exp(-energyJ / (boltz * Number(boltzmannTbox.value))));
 // cell potential, E = -dG/nF
-var cellpotentialbox = getBox("cellpotential");
-cellpotentialbox.toJoule = function () {
-    return (-Number(nelectronsbox.value) * faraday * Number(cellpotentialbox.value)) /
-        avogadro;
-};
-cellpotentialbox.fromJoule = function (energyJ) {
-    return (energyJ * avogadro) / (-Number(nelectronsbox.value) * faraday);
-};
+const cellpotentialbox = getBox("cellpotential");
+cellpotentialbox.toJoule = () => (-Number(nelectronsbox.value) * faraday * Number(cellpotentialbox.value)) /
+    avogadro;
+cellpotentialbox.fromJoule = (energyJ) => (energyJ * avogadro) / (-Number(nelectronsbox.value) * faraday);
 // Number of electrons (n) transferred in cell reaction: dG = -nFE
-var nelectronsbox = getBox("nelectrons");
-nelectronsbox.changed = function (energyJ) {
+const nelectronsbox = getBox("nelectrons");
+nelectronsbox.changed = (energyJ) => {
     cellpotentialbox.value = formatNumber((avogadro * energyJ) / (-Number(nelectronsbox.value) * faraday));
 };
 // British thermal units
-var btubox = getBox("btu");
-btubox.toJoule = function () { return Number(btubox.value) * btu; };
-btubox.fromJoule = function (energyJ) { return energyJ / btu; };
+const btubox = getBox("btu");
+btubox.toJoule = () => Number(btubox.value) * btu;
+btubox.fromJoule = (energyJ) => energyJ / btu;
 // Grams of TNT equivalent
-var tntbox = getBox("TNT");
-tntbox.toJoule = function () { return Number(tntbox.value) * tnt; };
-tntbox.fromJoule = function (energyJ) { return energyJ / tnt; };
+const tntbox = getBox("TNT");
+tntbox.toJoule = () => Number(tntbox.value) * tnt;
+tntbox.fromJoule = (energyJ) => energyJ / tnt;
 // Calories equivalent (kcal)
-var caloriebox = getBox("calorie");
-caloriebox.toJoule = function () { return Number(caloriebox.value) * kcal; };
-caloriebox.fromJoule = function (energyJ) { return energyJ / kcal; };
+const caloriebox = getBox("calorie");
+caloriebox.toJoule = () => Number(caloriebox.value) * kcal;
+caloriebox.fromJoule = (energyJ) => energyJ / kcal;
 // Rydberg units
-var rydbergbox = getBox("rydberg");
-rydbergbox.toJoule = function () { return Number(rydbergbox.value) * rydberg; };
-rydbergbox.fromJoule = function (energyJ) { return energyJ / rydberg; };
+const rydbergbox = getBox("rydberg");
+rydbergbox.toJoule = () => Number(rydbergbox.value) * rydberg;
+rydbergbox.fromJoule = (energyJ) => energyJ / rydberg;
 // Cgs standard units (ergs)
-var ergbox = getBox("erg");
-ergbox.toJoule = function () { return Number(ergbox.value) * erg; };
-ergbox.fromJoule = function (energyJ) { return energyJ / erg; };
+const ergbox = getBox("erg");
+ergbox.toJoule = () => Number(ergbox.value) * erg;
+ergbox.fromJoule = (energyJ) => energyJ / erg;
 // Barrels of oil equivalent
-var booebox = getBox("booe");
-booebox.toJoule = function () { return Number(booebox.value) * booe; };
-booebox.fromJoule = function (energyJ) { return energyJ / booe; };
+const booebox = getBox("booe");
+booebox.toJoule = () => Number(booebox.value) * booe;
+booebox.fromJoule = (energyJ) => energyJ / booe;
 // boxes that are energies
-var boxes = [
+const boxes = [
     freqbox,
     energyJbox,
     energyeVbox,
@@ -231,7 +203,7 @@ var boxes = [
     arrheniusbox,
 ];
 // boxes that aren't energies
-var nonEboxes = [
+const nonEboxes = [
     boltzmannTbox,
     boltzmanngbox,
     nelectronsbox,
@@ -240,27 +212,25 @@ var nonEboxes = [
 // function to update boxes when changed. 
 function update_form(event) {
     // changed box is the one that the event targeted
-    var changed_box = event.target;
+    const changed_box = event.target;
     // if it doesn't have toJoule method return
     if (!changed_box.toJoule)
         return;
     // calculate the energy in Joules from the changed box
-    var change_J = changed_box.toJoule(Number(changed_box.value));
+    const change_J = changed_box.toJoule(Number(changed_box.value));
     // run through all the other boxes and update them using the fromJoule functions
-    for (var _i = 0, boxes_2 = boxes; _i < boxes_2.length; _i++) {
-        var box = boxes_2[_i];
+    for (const box of boxes) {
         if (box === changed_box || !box.fromJoule)
             continue;
         box.value = formatNumber(box.fromJoule(change_J));
     }
 }
 // add event listeners for all the boxes, update_form for any energy. boxes
-for (var _i = 0, boxes_1 = boxes; _i < boxes_1.length; _i++) {
-    var box = boxes_1[_i];
+for (const box of boxes) {
     box.addEventListener("input", update_form);
 }
 // custom changed functions boxes that aren't directly energy units
-boltzmannTbox.addEventListener("input", function () { var _a; return (_a = boltzmannTbox.changed) === null || _a === void 0 ? void 0 : _a.call(boltzmannTbox, Number(energyJbox.value)); });
-boltzmanngbox.addEventListener("input", function () { var _a; return (_a = boltzmanngbox.changed) === null || _a === void 0 ? void 0 : _a.call(boltzmanngbox, Number(energyJbox.value)); });
-nelectronsbox.addEventListener("input", function () { var _a; return (_a = nelectronsbox.changed) === null || _a === void 0 ? void 0 : _a.call(nelectronsbox, Number(energyJbox.value)); });
-Aconstantbox.addEventListener("input", function () { var _a; return (_a = Aconstantbox.changed) === null || _a === void 0 ? void 0 : _a.call(Aconstantbox, Number(energyJbox.value)); });
+boltzmannTbox.addEventListener("input", () => { var _a; return (_a = boltzmannTbox.changed) === null || _a === void 0 ? void 0 : _a.call(boltzmannTbox, Number(energyJbox.value)); });
+boltzmanngbox.addEventListener("input", () => { var _a; return (_a = boltzmanngbox.changed) === null || _a === void 0 ? void 0 : _a.call(boltzmanngbox, Number(energyJbox.value)); });
+nelectronsbox.addEventListener("input", () => { var _a; return (_a = nelectronsbox.changed) === null || _a === void 0 ? void 0 : _a.call(nelectronsbox, Number(energyJbox.value)); });
+Aconstantbox.addEventListener("input", () => { var _a; return (_a = Aconstantbox.changed) === null || _a === void 0 ? void 0 : _a.call(Aconstantbox, Number(energyJbox.value)); });
---
+++ scripts/plot_script.ts
@@ -0,0 +1,231 @@
+//import Chart from "chart.js/auto";
+declare const Chart: any;
+const aslider = document.getElementById("aSlider") as HTMLInputElement;
+const aValue = document.getElementById("aValue") as HTMLSpanElement;
+const kslider = document.getElementById("kSlider") as HTMLInputElement;
+const kValue = document.getElementById("kValue") as HTMLSpanElement;
+const bslider = document.getElementById("bSlider") as HTMLInputElement;
+const bValue = document.getElementById("bValue") as HTMLSpanElement;
+const ctx = (document.getElementById("chart") as HTMLCanvasElement).getContext("2d")!;
+const attdropdown = document.getElementById("attdropdown") as HTMLSelectElement;
+const repdropdown = document.getElementById("repdropdown") as HTMLSelectElement;
+const presetdropdown = document.getElementById("presetdropdown") as HTMLSelectElement;
+
+const k_init = 1;
+const a_init = 0.2;
+const b_init = 1;
+
+
+function linspace(start: number, end: number, num: number): number[] {
+    const arr: number[] = [];
+    const step = (end - start) / (num - 1);
+    for (let i = 0; i < num; i++) {
+        arr.push(start + step * i);
+    }
+    return arr;
+}
+
+let rs: number[] = linspace(1E-6, 12, 1000);
+
+function DLVO_attraction(r: number[], a: number, deg: number = Number(attdropdown.value)) {
+    const ys: number[] = [];
+
+    for (let r of rs) {
+        ys.push( - a / r**deg) ;
+    }
+
+    return ys;
+}
+
+function hard_sphere_repulsion(r: number[]) {
+    const ys: number[] = [];
+
+    for (let r of rs) {
+        ys.push(1E-10 / r**12);
+    }
+
+    return ys;
+}
+
+function DLVO_repulsion(r: number[],b: number, k: number, potential: string = String(repdropdown.value)) {
+    const ys: number[] = [];
+
+    for (let r of rs) {
+        if (potential === "yukawa") {
+            ys.push(b*k*k*Math.exp(- r/k )/r);
+        } else {
+            ys.push(b*k*k*Math.exp(- r/k ));
+        }
+    }
+
+    return ys;
+}
+
+function combined_DLVO(hs: number[], att: number[], rep: number[]) {
+    const combined: number[] = att.map((val, idx) => val + rep[idx] + hs[idx]);
+    return combined;
+}
+
+let att  = DLVO_attraction(rs, a_init);
+let rep_DH = DLVO_repulsion(rs, b_init, k_init);
+let rep_HS = hard_sphere_repulsion(rs);
+let combined = combined_DLVO(rep_HS, att, rep_DH);
+
+const chart = new Chart(ctx, {
+    type: "line",
+    data: {
+        labels: rs,
+        datasets: [{
+            label: 'Attractive Interactions',
+            data: att,
+            borderWidth: 1,
+            borderDash: [5, 5],
+            fill: false,
+            borderColor: '#00D118',
+            backgroundColor: '#00D118'
+
+        }, {
+            label: 'Repulsive Interactions',
+            data: rep_DH,
+            borderWidth: 1,
+            borderDash: [5, 5],
+            fill: false,
+            borderColor: '#FF0015'
+        },
+        {
+            label: "Hard Sphere Repulsion",
+            data: rep_HS,
+            borderWidth: 1,
+            borderDash: [5, 5],
+            fill: false,
+            borderColor: '#FFAE00'
+        },
+        {
+            label: "Combined DLVO Potential",
+            data: combined,
+            borderWidth: 2,
+            fill: false,
+            borderColor: '#0015FF'
+        }]
+    },
+    options: {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'top', labels: {
+                    pointStyle: 'line',
+                    usePointStyle: true,
+                }}},
+        animation: false,
+        scales: {
+            x: {
+                title: { display: true, text: "Distance Between Particles" },
+                ticks: { display: false },
+                grid: { display: false }
+            },
+            y: {
+                title: { display: true, text: "DLVO Energy (V_DLVO)" },
+                min: -0.2,
+                max: 0.3,
+                afterBuildTicks: (axis: { ticks: { value: number; }[]; }) => axis.ticks = [0].map(v => ({ value: v })) 
+            }
+        },
+        elements: {
+            point: {
+                radius: 0
+            }
+        }   
+    }
+});
+
+function updateGraph(a: number, b: number, k: number, deg: number = Number(attdropdown.value), rep_potential: string = repdropdown.value) {
+    const data_att = DLVO_attraction(rs, a, deg);
+    const data_rep = DLVO_repulsion(rs, b, k, rep_potential);
+  //  chart.data.labels = data.rs;
+    chart.data.datasets[0].data = data_att;
+    chart.data.datasets[1].data = data_rep;
+    chart.data.datasets[3].data = combined_DLVO(rep_HS, data_att, data_rep);
+    chart.update();
+}
+
+aslider.addEventListener("input", () => {
+    const a = parseFloat(aslider.value);
+    const b = parseFloat(bslider.value);
+    const k = parseFloat(kslider.value);
+    aValue.textContent = a.toString();
+    updateGraph(a, b, k);
+});
+
+
+kslider.addEventListener("input", () => {
+    const k = parseFloat(kslider.value);
+    const a = parseFloat(aslider.value);
+    const b = parseFloat(bslider.value);
+    kValue.textContent = k.toString();
+    updateGraph(a, b, k);
+});
+
+bslider.addEventListener("input", () => {  
+    const b = parseFloat(bslider.value);
+    const a = parseFloat(aslider.value);
+    bValue.textContent = b.toString();
+    const k = parseFloat(kslider.value);
+    updateGraph(a, b, k);
+});
+
+attdropdown.addEventListener("change", () => {
+    const a = parseFloat(aslider.value);
+    const b = parseFloat(bslider.value);
+    const k = parseFloat(kslider.value);
+    const deg = Number(attdropdown.value);
+    updateGraph(a, b, k, deg);
+});
+
+repdropdown.addEventListener("change", () => {
+    const a = parseFloat(aslider.value);
+    const b = parseFloat(bslider.value);
+    const k = parseFloat(kslider.value);
+    const deg = Number(attdropdown.value);
+    const rep_potential = repdropdown.value;
+    updateGraph(a, b, k, deg, rep_potential);
+});
+
+presetdropdown.addEventListener("change", () => {
+    const preset = presetdropdown.value;
+    if (preset === "flocculated") {
+        aslider.value = 0.4.toString();
+        bslider.value = 3.5.toString();
+        kslider.value = 0.7.toString();
+        attdropdown.value = "2";
+        repdropdown.value = "exponential";
+        const deg = Number(attdropdown.value);
+        const a = parseFloat(aslider.value);
+        const b = parseFloat(bslider.value);
+        const k = parseFloat(kslider.value);
+        updateGraph(a, b, k, deg, repdropdown.value);
+    } 
+    else if (preset === "coagulated") { 
+        aslider.value = 0.4.toString();
+        bslider.value = 3.5.toString();
+        kslider.value = 0.5.toString();
+        attdropdown.value = "2";
+        repdropdown.value = "exponential";
+        const deg = Number(attdropdown.value);
+        const a = parseFloat(aslider.value);
+        const b = parseFloat(bslider.value);
+        const k = parseFloat(kslider.value);
+        updateGraph(a, b, k, deg, repdropdown.value);
+
+    }
+    else if (preset === "stable") { 
+        aslider.value = 0.8.toString();
+        bslider.value = 1.2.toString();
+        kslider.value = 2.0.toString();
+        attdropdown.value = "2";
+        repdropdown.value = "exponential";
+        const deg = Number(attdropdown.value);
+        const a = parseFloat(aslider.value);
+        const b = parseFloat(bslider.value);
+        const k = parseFloat(kslider.value);
+        updateGraph(a, b, k, deg, repdropdown.value);
+    } } );
